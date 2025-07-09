1ed0b880009e135bbcf73bc137d891c6cbd6e6f0
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-07-09 16:09:42 +0500

    add array initialization and sum calculation in prac9 module

--- src/prac9.rs
+++ src/prac9.rs
@@ -1,6 +1,10 @@
 fn main(){
     println!("Array's Practice!");
-    let arr1: [u8; 5];
+    let arr1: [u8; 5]; // Declare an array of 5 elements of type u8
     arr1 = [1,2,3,4,5];
     println!("{:#?}", arr1);
+    let arr2: [u8; 5] = [6,7,8,9,10]; //initialization of an array with values
+    let sum: u8 = arr1[0] + arr2[1];
+    println!("Sum of the elements of the array is {}", sum);
+    let _arr3: [u8; 5];
 }
