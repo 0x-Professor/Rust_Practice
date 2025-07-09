08be718fe65681001dbb9a49ad173a58b1b03773
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-07-09 16:31:40 +0500

    add array manipulation functions in prac9 module

--- src/prac9.rs
+++ src/prac9.rs
@@ -7,4 +7,21 @@ fn main(){
     let sum: u8 = arr1[0] + arr2[1];
     println!("Sum of the elements of the array is {}", sum);
     let _arr3: [u8; 5];
+    let mut arr: [&str; 2] = ["Hello", "Blockchain!"]; // Declare an array of 2 elements of type &str
+    write_array(arr);
+    write_array2(&mut arr);
+    println!("original Array {:#?}", arr);
+    
+}
+fn write_array(mut arr1: [&str; 2]){
+    arr1[0] = "Hello, Rust!";
+    arr1[1] = "Welcome to Blockchain!";
+    println!("Modified array: {:#?}", arr1);
+}
+// Another way to write the array without creating their clone
+
+fn write_array2(arr2:&mut [&str; 2] ){
+    arr2[0] = "Hello, Buddy!";
+    arr2[1] = "Enjoy Blockchain!";
+    println!("Modified array2: {:#?}", arr2);
 }
