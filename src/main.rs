3ee09de577f36f1924e1bb554b138fd83ca7b8c0
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-07-09 13:42:19 +0500

    initialize Rust project with Cargo.toml and main.rs, add .gitignore for target directory

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod prac1;
+mod prac3;
+mod prac4;
+mod prac5;
+mod prac6;
+
+fn main(){
+    let string_literal:&str = "Hello, Blockchain!";
+    println!("{}", string_literal);
+    let mut string_var:String = String::from("Happy Coding!");
+    println!("{}", string_var);
+    string_var.push_str(" Enjoy Rust!");
+    println!("{}", string_var);
+    let employee_info:(&str, u8) = ("Mazhar", 20);
+    let (name, age) = employee_info;
+    println!("{} is {} years old.", name, age);
+    print_value(7);
+    let sum:u8 = add(12, 18);
+    println!("Sum: {}", sum);
+}
+fn print_value(value:u8){
+    println!("Value: {}", value);
+    
+}
+fn add(x:u8, y:u8) -> u8 {
+    return x + y
+    
+}
