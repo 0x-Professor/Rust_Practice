e27e90f2f0ccd8d4e3a8bb66774763c6b77ecdff
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-07-09 17:18:13 +0500

    update prac10 module with vector modification and new write_vector function

--- src/prac10.rs
+++ src/prac10.rs
@@ -7,5 +7,13 @@ fn main(){
     vec1.push(9);
     println!("{:?}", vec1);
     println!("{:?}", vec2);
+    let mut vrr: Vec<i32> = vec![1, 2, 3, 4, 5];
+    write_vector(&mut vrr);
+    println!("{:?}", vrr);
+}
+fn write_vector(vec: &mut Vec<i32>){
+    vec.pop(); // Remove the last element from the vector
+    vec.push(6);
+    println!("{:?}", vec);
     
 }
