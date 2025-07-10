80c0c1c725bf80dde8d40dc59a4e64786704c3c4
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-07-10 07:46:25 +0500

    add func_match function to prac12 module for number matching

--- src/prac12.rs
+++ src/prac12.rs
@@ -9,4 +9,18 @@ fn main(){
     else{
         println!("number is equal to 10");
     }
+    func_match();
+}
+
+fn func_match(){
+    let number:u8 = 3;
+    match number{
+        0 => println!("number is 0"),
+        1 => println!("number is 1"),
+        2 => println!("number is 2"),
+        3 => println!("number is 3"),
+        4 => println!("number is 4"),
+        5 => println!("number is 5"),
+        _ => println!("number is not 0-5")
+    }
 }
