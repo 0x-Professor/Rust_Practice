99f4d5ad3cab33cdd48a89095b002204e6320362
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-07-10 09:18:43 +0500

    add additional random number generation examples

--- src/random_number.rs
+++ src/random_number.rs
@@ -3,4 +3,10 @@ fn main(){
     let mut rng = rand::thread_rng();
     let random_number:u32 = rng.gen_range(1..100);
     println!("{}",random_number);
+    let random_int:i32 = rng.r#gen();
+    println!("{}",random_int);
+    let random_float:f64 = rng.r#gen();
+    println!("{}",random_float);
+    let random_bool:bool = rng.r#gen();
+    println!("{}",random_bool);
 }
